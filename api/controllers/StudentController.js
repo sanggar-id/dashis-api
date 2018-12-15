ca8e2930651656d3f595408faf3d5a5df3743aa4
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-16 02:11:56 +0700

    add login in Student Controller

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -6,7 +6,69 @@
  */
 
 module.exports = {
-  
+	login = (req, res) => {
+		let email = req.param('email');
+		let password = req.param('password');
+		
+		//untuk login, email dan password harus di isi
+		if (!email || !password) {
+			/**
+			 * jika kosong, tampilkan respon berikut dengan kode 401
+			 * kode 401 merepresentasikan `not found`
+			 */
+			return res.json(401, {
+				code: 401,
+				message: 'email or password required'
+			});
+		}
 
+		Student.findOne({email: email}, (err, student) => {
+			/**
+			 * validasi dibawah untuk mengecek, apakah user dengan `email`
+			 * yang dimasukkan terdaftar didalam database atau tidak
+			 */
+			if (!student) return res.json(401, {
+				code: 401,
+				message: 'email or password required'
+			});
+
+			/**
+			 * selanjutkan, melakukan validasi untuk melakukan komparasi
+			 * dari password yang dimasukan dengan password yang sudah terdaftar
+			 * didalam database
+			 */
+			Student.comparePassword(password, student, (err, valid) => {
+				/**
+				 * lakukan validasi jika password yang dimasukkan-
+				 * tidak sama yang ada di database.
+				 * kode 403 merepresentasikan `forbidden`
+				 */
+				if (err) return res.json(403, {
+					code: 403,
+					message: 'email or password required'
+				});
+
+				/**
+				 * lakukan validasi terakhir untuk memastikan autentikasi
+				 * tidak dapat di bypass.
+				 */
+				if (!valid) {
+					return res.json(401, {
+						code: 401,
+						message: 'email or password required'
+					});
+				} else {
+					/**
+					 * jika login berhasil, tampilkan response OK dengan kode 200.
+					 */
+					return res.json(200, {
+						code: 200,
+						mahasiswa: student,
+						token: jwToken.issue({id: student.id})
+					});
+				}
+			});
+		});
+	}
 };
 
