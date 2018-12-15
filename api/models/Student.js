7b18af3c6707c9f2872feeba2e2a65f301153a8c
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-16 01:46:35 +0700

    add student API with blueprint

--- api/models/Student.js
+++ api/models/Student.js
@@ -4,6 +4,7 @@
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -22,7 +23,34 @@ module.exports = {
     },
     password: {
       type: 'string'
+    },
+    // We don't wan't to send back encrypted password either
+    toJSON: function () {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
     }
   },
+  beforeCreate: function (values, next) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(values.password, salt, function (err, hash) {
+        if (err) return next(err);
+        values.password = hash;
+        next();
+      })
+    })
+  },
 
+  comparePassword: function (password, user, cb) {
+    bcrypt.compare(password, user.password.replace('$2y$', '$2a$'), function (err, match) {
+
+      if (err) cb(err);
+      if (match) {
+        cb(null, true);
+      } else {
+        cb(err);
+      }
+    })
+  }
 };
