7406c9cf566ee486b148ace937097e120cd176dd
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-16 01:56:32 +0700

    student model, done

--- api/models/Student.js
+++ api/models/Student.js
@@ -9,48 +9,44 @@ var bcrypt = require('bcrypt');
 module.exports = {
 
   primaryKey: 'id',
+
   attributes: {
-    id: {
-      type: 'number',
-      autoIncrement: true
-    },
-    name: {
-      type: 'string'
-    },
-    email: {
-      type: 'string',
-      unique: true
-    },
-    password: {
-      type: 'string'
-    },
-    // We don't wan't to send back encrypted password either
-    toJSON: function () {
-      var obj = this.toObject();
-      delete obj.password;
-      return obj;
-    }
+
+    id          : { type: 'number', autoIncrement: true },
+    name        : { type: 'string' },
+    email       : { type: 'string', unique: true },
+    password    : { type: 'string' },
+    createdAt   : { type: 'number', autoCreatedAt: true },
+    updatedAt   : { type: 'number', autoUpdatedAt: true },
+
   },
-  beforeCreate: function (values, next) {
-    bcrypt.genSalt(10, function (err, salt) {
-      if (err) return next(err);
-      bcrypt.hash(values.password, salt, function (err, hash) {
+  
+    //ketika kueri ke student, jangan tampilkan data yang sensitif
+    customToJSON: function () {
+        return _.omit(this, ['id', 'name', 'email', 'createdAt'])
+    },
+
+    //encrypt password menggunakan bcrypt
+    beforeCreate: function (values, next) {
+        bcrypt.genSalt(10, function (err, salt) {
         if (err) return next(err);
-        values.password = hash;
-        next();
-      })
-    })
-  },
+        bcrypt.hash(values.password, salt, function (err, hash) {
+            if (err) return next(err);
+            values.password = hash;
+            next();
+        })
+        })
+    },
 
-  comparePassword: function (password, user, cb) {
-    bcrypt.compare(password, user.password.replace('$2y$', '$2a$'), function (err, match) {
-
-      if (err) cb(err);
-      if (match) {
-        cb(null, true);
-      } else {
-        cb(err);
-      }
-    })
-  }
+    //keperluan untuk login
+    comparePassword: function (password, user, cb) {
+        bcrypt.compare(password, user.password.replace('$2y$', '$2a$'), function (err, match) {
+        if (err) cb(err);
+        if (match) {
+            cb(null, true);
+        } else {
+            cb(err);
+        }
+        })
+    }
 };
